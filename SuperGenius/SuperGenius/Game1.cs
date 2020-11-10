3468f10cb648c98434f9ab7ae339fe5baa80a2ca
Rishik Yechuri <56663764+Rishik-Yechuri@users.noreply.github.com>
2020-11-10 09:12:27 -0800

    Can control two characters
    -Can control road runner with arrow keys
    -Can control Coyote with WASD

--- SuperGenius/SuperGenius/Game1.cs
+++ SuperGenius/SuperGenius/Game1.cs
@@ -19,6 +19,16 @@ namespace SuperGenius
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
+        Texture2D roadRunner;
+        Rectangle coyoteRectangle;
+
+        Texture2D coyote;
+        Rectangle runnerRectangle;
+
+        int xLocMouse = 100;
+        int yLocMouse = 100;
+        int xLocRunner = 300;
+        int yLocRunner = 300;
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
@@ -34,7 +44,8 @@ namespace SuperGenius
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            runnerRectangle = new Rectangle(xLocRunner, yLocRunner, 125, 125);
+            coyoteRectangle = new Rectangle(xLocMouse, yLocMouse, 92, 79);
             base.Initialize();
         }
 
@@ -48,6 +59,8 @@ namespace SuperGenius
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            roadRunner = this.Content.Load<Texture2D>("Roadrunner");
+            coyote = this.Content.Load<Texture2D>("Wile E Coyote");
         }
 
         /// <summary>
@@ -71,7 +84,41 @@ namespace SuperGenius
                 this.Exit();
 
             // TODO: Add your update logic here
-
+            KeyboardState kb = Keyboard.GetState();
+            if (kb.IsKeyDown(Keys.W))
+            {
+                yLocRunner -= 2;
+            }
+            if (kb.IsKeyDown(Keys.S))
+            {
+                yLocRunner += 2;
+            }
+            if (kb.IsKeyDown(Keys.A))
+            {
+                xLocRunner -= 2;
+            }
+            if (kb.IsKeyDown(Keys.D))
+            {
+                xLocRunner += 2;
+            }
+            if (kb.IsKeyDown(Keys.Left))
+            {
+                xLocMouse -= 3;
+            }
+            if (kb.IsKeyDown(Keys.Right))
+            {
+                xLocMouse += 3;
+            }
+            if (kb.IsKeyDown(Keys.Up))
+            {
+                yLocMouse -= 3;
+            }
+            if (kb.IsKeyDown(Keys.Down))
+            {
+                yLocMouse += 3;
+            }
+            runnerRectangle = new Rectangle(xLocMouse, yLocMouse, 92, 79);
+            coyoteRectangle = new Rectangle(xLocRunner, yLocRunner, 125, 125);
             base.Update(gameTime);
         }
 
@@ -84,7 +131,10 @@ namespace SuperGenius
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
-
+            spriteBatch.Begin();
+            spriteBatch.Draw(roadRunner, runnerRectangle, Color.White);
+            spriteBatch.Draw(coyote, coyoteRectangle, Color.White);
+            spriteBatch.End();
             base.Draw(gameTime);
         }
     }
